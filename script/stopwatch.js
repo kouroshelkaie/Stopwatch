11480546a914f0e709fa75e423669982357c5ca0
Kourosh Elkaie <you@example.com>
2021-07-07 09:03:12 +0430

    startCounter function

--- script/stopwatch.js
+++ script/stopwatch.js
@@ -9,3 +9,34 @@ const lapClear = document.getElementById("clear")
 let progress = document.getElementById('progress')
 
 let sec = 00; milli = 00;
+
+
+
+function startCounter() {
+    InterV = setInterval(()=> {
+        milli++
+        milliSecond.innerHTML = milli;
+        proW = progress.offsetWidth;
+        progress.style.width = (proW+17)+"px"
+        if(milli>=99) {
+            milli = 0
+            sec++
+            progress.style.width = 0;
+            if(sec<10) {
+                second.innerHTML = "0"+sec;
+            }
+            else {
+                second.innerHTML = sec
+            }
+        }
+        stp.addEventListener("click",()=> {
+            clearInterval(InterV)
+        })
+        reset.addEventListener("click",()=>{
+          clearInterval(InterV);
+          second.innerHTML = "00"
+          milliSecond.innerHTML = "00"
+          progress.style.width =0
+        })
+    },10)
+}
