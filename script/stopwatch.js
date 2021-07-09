03432ec655218ea8c6b0c9cafbbd0c085542c2e2
Kourosh Elkaie <you@example.com>
2021-07-09 22:44:57 +0430

    counter_lap capped

--- script/stopwatch.js
+++ script/stopwatch.js
@@ -11,3 +11,70 @@ let lapCounter = 0;
 let sec = 00;
 milli = 00;
 min = 00;
+
+
+function startCounter() {
+    InterV = setInterval(() => {
+      milli++;
+      if (milli < 10) {
+        milliSecond.innerHTML = `0${milli}`;
+      } else {
+        milliSecond.innerHTML = milli;
+      }
+      if (milli >= 99) {
+        milli = 0;
+        sec++;
+        if (sec < 10) {
+          second.innerHTML = `0${sec}`;
+        } else {
+          second.innerHTML = sec;
+        }
+      }
+      if (sec > 59) {
+        sec = 00;
+        min++;
+        if (min < 10) {
+          minute.innerHTML = `0${min}`;
+        } else {
+          minute.innerHTML = min;
+        }
+      }
+      stp.addEventListener("click", () => {
+        clearInterval(InterV);
+      });
+      reset.addEventListener("click", () => {
+        clearInterval(InterV);
+        sec = 0;
+        milli = 0;
+        min = 0;
+        second.innerHTML = "00";
+        milliSecond.innerHTML = "00";
+        minute.innerHTML = "00";
+      });
+    }, 10);
+  }
+  
+  start.addEventListener("click", startCounter);
+  
+  lap.addEventListener("click", () => {
+    lapCounter++;
+    let secSplit = second.innerText;
+    let millSplit = milliSecond.innerHTML;
+    let minSplit = minute.innerHTML;
+  
+    let lapItem = document.createElement("li");
+    let lapSpan = document.createElement("small");
+    let itemSpan = document.createElement("span");
+  
+    lapSpan.innerHTML = `lap ${lapCounter}`;
+    itemSpan.innerHTML = `${minSplit}:${secSplit}:${millSplit}`;
+    lapItem.appendChild(lapSpan);
+    lapItem.appendChild(itemSpan);
+    lapContainer.appendChild(lapItem);
+  });
+  
+  lapClear.addEventListener("click", () => {
+    lapContainer.innerHTML = " ";
+    lapCounter = 0;
+  });
+  
